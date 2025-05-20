d900891e1286e74b900a68bccb379bb5657e230e
bestdevelope <leabouillot7@gmail.com>
2025-05-20 16:49:02 +0200

    Loading image

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,24 @@
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Login from "./routes/Login";
-import Join from "./routes/Join";
-import Main from "./routes/Main";
-import Order from "./routes/Order";
-import MyOrder from "./routes/MyOrder";
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
-function App() {
+import Join from './routes/Join';
+import Main from './routes/Main';
+import MyOrder from './routes/MyOrder';
+import Order from './routes/Order';
+import Loading from './components/Login/Loading';
+
+const App: React.FC = () => {
   return (
     <Router>
-      <Route path="/" exact component={Login} />
-      <Route path="/join" component={Join} />
-      <Route path="/main" component={Main} />
-      <Route path="/myorder" component={MyOrder} />
-      <Route path="/order" component={Order} />
+      <Routes>
+        <Route path="/" element={<Loading />} />
+        <Route path="/join" element={<Join />} />
+        <Route path="/main" element={<Main />} />
+        <Route path="/myorder" element={<MyOrder />} />
+        <Route path="/order" element={<Order />} />
+      </Routes>
     </Router>
   );
-}
+};
 
 export default App;
---
+++ src/components/Login/Loading.tsx
@@ -0,0 +1,10 @@
+
+export default function Loading() {
+  return (
+    <>
+    <h1>Lunch Box App</h1>
+    <img src="/lunchbox.jpg" alt="icon lunch box app" height={500}
+    />
+    </>
+  )
+}
