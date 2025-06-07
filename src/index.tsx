af2ad3d296050659c81c1470b996c882afafb960
bestdevelope <leabouillot7@gmail.com>
2025-06-07 18:29:15 +0200

    apollo client

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,24 @@
+// index.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/', // URL de serveur GraphQL Apollo
+  cache: new InMemoryCache(),
+});
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>
 );
